9e862a9a5869e5a24d4936a23e9a21eed3931e12
Katherine Kerman <tonnam_s@outlook.com>
2024-04-17 15:45:33 +0700

    new file:   homeworks/clockOps.py

---
+++ homeworks/clockOps.py
@@ -0,0 +1,6 @@
+timeInMin = int(input())
+timeInHrs = timeInMin//60
+trueMin = timeInMin - (timeInHrs*60)
+print(timeInHrs,"\n",trueMin)
+
+#Yes I know I could just use %, but I'm too lazy to change my code now.
