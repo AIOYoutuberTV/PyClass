9bc0fbe62f78ae3041cc929b89671ce686436ead
Katherine Kerman <tonnam_s@outlook.com>
2024-04-18 20:14:17 +0700

    Co-authored-by: Katherine Kerman <tonnam_s@outlook.com>

--- amongus.py
+++ amongus.py
@@ -1,2 +1,15 @@
 import turtle as tortoise
 
+"""tortoise.screensize(600,600)
+tortoise.window_height();tortoise.window_width()
+tortoise.up();tortoise.setx(550);tortoise.sety(550);tortoise.down()"""
+tortoise.right(90)
+tortoise.forward(200)
+tortoise.circle(50,180)
+tortoise.forward(100)
+tortoise.right(180)
+tortoise.forward(100)
+tortoise.circle(50,180)
+tortoise.forward(135)
+
+tortoise.done()
