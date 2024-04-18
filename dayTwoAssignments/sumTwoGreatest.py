56b78a6455dcdc6048a7683ee1945f67194277f5
Katherine Kerman <tonnam_s@outlook.com>
2024-04-18 23:10:08 +0700

    Branched

--- dayTwoAssignments/sumTwoGreatest.py
+++ dayTwoAssignments/sumTwoGreatest.py
@@ -1,8 +1,16 @@
 treasure = [int(input()) , int(input()) , int(input())]
 
-if treasure[0] < treasure[1] and treasure[0] < treasure[2]:
-    print(treasure[1]+treasure[2])
-elif treasure[0] > treasure[1] and treasure[0] < treasure[2]:
-    print(treasure[0]+treasure[2])
-else:
-    print(treasure[0]+treasure[1])
+i = 0
+rm1 = 0
+rm2 = 0
+while i < len(treasure):
+    if rm1 < treasure[i]:
+        rm2 = rm1
+        rm1 = treasure[i]
+    elif rm2 < treasure[i]:
+        rm2 = treasure[i]
+    else:
+        break
+    i += 1
+#Why am I manually managing each va7r like I'm working with register in assembly? 
+print(rm1+rm2)
