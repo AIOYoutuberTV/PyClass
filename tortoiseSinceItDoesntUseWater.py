3e3fa33c340665074f1814b0a61a9a232a69a3e1
Katherine Kerman <tonnam_s@outlook.com>
2024-04-18 15:19:01 +0700

    Changes to be committed: 	modified:   tortoiseSinceItDoesntUseWater.py
    	UPDATE: Minor indentation fixes

--- tortoiseSinceItDoesntUseWater.py
+++ tortoiseSinceItDoesntUseWater.py
@@ -7,12 +7,14 @@ colourEnd = 0xFF170F
 colourDiff = 0x8E07//20 #For whatever reason python stores these as int in decimal... Okay???
 
 def hexToColourCode(inColour): #Takes the int data and turns it into a string with the format usable by the tortoise.
-    return("#"+(hex(inColour)[2:]))
+	return("#"+(hex(inColour)[2:]))
 
 tortoise.width(5)
 tortoise.color(hexToColourCode(colourStart))
 tortoise.up();tortoise.setx(-350);tortoise.down()
 while colourStart != colourEnd:
-    tortoise.forward(2)
-    tortoise.color(hexToColourCode(colourStart-colourDiff)) 
+	tortoise.forward(2)
+	tortoise.color(hexToColourCode(colourStart-colourDiff)) 
 tortoise.done()
+
+#TODO: Figure out whatever is going on here
